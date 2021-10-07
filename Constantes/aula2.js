fa932f58cff2855181ae9d650c285e77e64f3db8
Lucas Matheus <88047154+Lucasmtrs@users.noreply.github.com>
2021-10-07 12:46:51 -0300

    added files
    added directories: variáveis & constantes

---
+++ Constantes/aula2.js
@@ -0,0 +1,8 @@
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 40;
+console.log(valorIngressoAdulto);
+
+const altura = 165;
+altura = 180;
+console.log(altura);
+
---
+++ Variaveis/aula.js
@@ -0,0 +1,12 @@
+// console.log('Lucas')
+// console.log('bem vindo ao curso')
+// let idade = 5;
+// console.log(idade)
+// let altura = 165;
+// console.log(altura)
+let car = "toyota";
+console.log(car)
+let plane = "embraer";
+console.log(plane)
+let mountainName = "everest";
+console.log(mountainName)
