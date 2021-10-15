51e869009f7b8caa0e426de1abf8315690c154d0
Lucas Matheus <lucasmthrs@gmail.com>
2021-10-15 15:10:42 -0300

    added exercícios

---
+++ Exercícios/ex1.js
@@ -0,0 +1,25 @@
+// como trocar valores de variáveis, pegar valor de uma variável e modoficar por outro;
+
+let a = 'vermelho';
+let b = 'azul';
+
+a = 'azul';
+b = 'vermelho';
+
+console.log(a);
+console.log(b); //minha resolução;
+
+
+//resolução do professor:
+
+let c = 'rosa';
+let d = 'verde';
+
+let e = c;
+c = d;
+d = e;
+
+console.log(c);
+console.log(d); //invertidos;
+
+
