a184d2f609a61dd52336ff5e2b5de5d2a45153a8
Lucas Matheus <lucasmthrs@gmail.com>
2021-10-14 21:08:29 -0300

    Operators

---
+++ Operadores/aula8.js
@@ -0,0 +1,39 @@
+// Operadores Aritiméticos (matemáticos)
+// Operadores de Atribuição
+// Operadores de Comparação
+// Operadores Lógicos
+// Operadores Bitwise
+
+//Operadores aritiméticos: 
+let salario = 100;
+
+// + , - , * , / , **(o número vezes ele mesmo, exponencial)
+
+console.log(salario + salario);
+console.log(salario - 50);
+console.log(salario * salario);
+console.log(salario / 5);
+console.log(salario ** salario);
+
+
+// ++ e -- 
+// São operadores de INCREMENTO e DECREMENTO:
+let idade = 18;
+
+console.log(++idade);  // adiciona 1 ao número
+console.log(--idade);  // retira 1 do número
+
+
+
+// Operadores de Atribuição:
+let valorTecladoGamer = 200;
+console.log(valorTecladoGamer);
+
+// o sinal = é um operador de atribuição
+// o sinal += é um operador que significa: valorTecladoGamer =  valorTecladoGamer +  valorTecladoGamer;
+valorTecladoGamer += valorTecladoGamer;
+console.log(valorTecladoGamer);
+
+// -= significa x = x - x
+valorTecladoGamer -= valorTecladoGamer;
+console.log(valorTecladoGamer);
