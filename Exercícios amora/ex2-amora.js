c8c2f82a0ce2883c34a07a3fa903b1fba8caafe8
Lucas Matheus <lucasmthrs@gmail.com>
2021-11-19 00:11:08 -0300

    add function exercises from trello

--- Exercícios amora/ex2-amora.js
+++ Exercícios amora/ex2-amora.js
@@ -9,3 +9,35 @@ add(2048,1024);
 
 console.log(add()); 
 */
+
+let x = 7;
+let y = 15;
+
+function changeVarValue() {
+    let z = x;
+    x = y;
+    y = z;
+    return {x,y}
+    
+};
+
+console.log(changeVarValue(x, y));
+
+/*
+var arrayMax = [45,28,36,54,63,52,85];
+
+
+
+
+
+let arrayDesorganizado = [5,6,8,9,7,3,2,1,4];
+
+function retornaArrayOrdenado(x,y){
+    if (x == y) return 0;
+    if (x < y) return -1;
+    if (x > y) return 1;
+};
+
+arrayDesorganizado.sort(retornaArrayOrdenado); 
+
+*/
